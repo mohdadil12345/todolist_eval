3b9a1fca625599e99d53bb322050461bf8c47097
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-23 20:34:48 +0530

    post input data in json-server

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { TodoServiceService } from './service/todo-service.service';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,41 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
-  username: string = '';
+  title: string = '';
+
+
+  users: any;
+
+  constructor(private servc: TodoServiceService) {}
 
   handle_form(ele: NgForm) {
 
   console.log(ele.value)
+
+
+  this.users = {
+    title: ele.value.title,
+
+  };
+
+  console.log('usersss', this.users); // isko post krenge url me
+
+  // post
+
+this.servc.postInputData(this.users).subscribe((data) => {
+  alert('added data');
+  console.log('data', data);
+})
+
+
+  // this.servc.postFormData(this.users).subscribe((data) => {
+  //   alert('added data');
+  //   console.log('data', data);
+
+  //    ele.resetForm()
+
+  // });
+
 
   }
 
--- todolist/src/app/service/todo-service.service.ts
+++ todolist/src/app/service/todo-service.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class TodoServiceService {
 
-  constructor() { }
+  constructor( private http : HttpClient) { }
+
+  url = "http://localhost:3000/todos"
+
+// post
+
+postInputData(data : any) {
+  return this.http.post(this.url,  data)
+}
+
+
+
+
 }
